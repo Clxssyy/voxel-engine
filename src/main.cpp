f68a4af267ef62f9a6eca6c0da5cdcdebd433adb
Michael Connolly <mpc63@uakron.edu>
2024-04-07 17:48:58 -0400

    Add basic window rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,38 @@
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
 #include <iostream>
 
 int main() {
-  std::cout << "Hello, world!" << std::endl;
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW" << std::endl;
+    return 1;
+  }
+
+  GLFWwindow* window = glfwCreateWindow(800, 600, "Hello, world!", nullptr, nullptr);
+  if (!window) {
+    std::cerr << "Failed to create window" << std::endl;
+    glfwTerminate();
+    return 1;
+  }
+
+  glfwMakeContextCurrent(window);
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cerr << "Failed to initialize GLAD" << std::endl;
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 1;
+  }
+
+  glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+
+  do {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  } while (!glfwWindowShouldClose(window));
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
   return 0;
 }
