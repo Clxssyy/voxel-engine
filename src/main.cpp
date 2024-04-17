eb8c464173dbea0a8e263721bd78ba2cd44ffd53
Michael Connolly <mpc63@uakron.edu>
2024-04-17 17:13:09 -0400

    Add comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,20 @@
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 #include <iostream>
+#include "shader.hpp"
 
 int main() {
+  // Glfw initialization
   if (!glfwInit()) {
     std::cerr << "Failed to initialize GLFW" << std::endl;
     return 1;
   }
 
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);   
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // Window creation
   GLFWwindow* window = glfwCreateWindow(800, 600, "Hello, world!", nullptr, nullptr);
   if (!window) {
     std::cerr << "Failed to create window" << std::endl;
@@ -17,6 +24,8 @@ int main() {
   }
 
   glfwMakeContextCurrent(window);
+
+  // Glad initialization
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     std::cerr << "Failed to initialize GLAD" << std::endl;
     glfwDestroyWindow(window);
@@ -24,14 +33,16 @@ int main() {
     return 1;
   }
 
-  glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+  glClearColor(0.5f, 0.0f, 0.7f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
+  glfwSwapBuffers(window);
 
+  // Main loop
   do {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
     glfwPollEvents();
-  } while (!glfwWindowShouldClose(window));
+  } while (!glfwWindowShouldClose(window) && !glfwGetKey(window, GLFW_KEY_ESCAPE));
 
+  // Cleanup
   glfwDestroyWindow(window);
   glfwTerminate();
   return 0;
